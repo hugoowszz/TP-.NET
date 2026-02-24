1f8c44d9c3c399d702773aacde6bc43b8bc3029b
Victor Hugo <hugoowvicttor@gmail.com>
2026-02-24 18:30:17 -0300

    Atividades 4 e 5

---
+++ 4/4/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace _4;
+
+class Program
+{
+    public class TemperatureEventArgs : EventArgs
+    {
+        public double Temperature { get; }
+
+        public TemperatureEventArgs(double temperature)
+        {
+            Temperature = temperature;
+        }
+    }
+
+    public class TemperatureSensor
+    {
+        public event EventHandler<TemperatureEventArgs> TemperatureExceeded;
+
+        public void verificarTemperatura(double temperatura)
+        {
+            Console.WriteLine("Temperatura atual: " + temperatura);
+
+            if (temperatura > 100)
+            {
+                OnTemperatureExceeded(new TemperatureEventArgs(temperatura));
+            }
+        }
+
+        protected virtual void OnTemperatureExceeded(TemperatureEventArgs e)
+        {
+            TemperatureExceeded?.Invoke(this, e);
+        }
+    }
+    
+    static void Main(string[] args)
+    {
+        TemperatureSensor sensor = new TemperatureSensor();
+
+        sensor.TemperatureExceeded += (sender, e) =>
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Temperatura excedida!");
+            Console.ResetColor();
+        };
+
+        while (true)
+        {
+            Console.WriteLine("Informe a temperatura atual: \nDigite 'sair' para cancelar");
+            string tempAtual = Console.ReadLine();
+
+            if (tempAtual.ToLower() == "sair")
+            {
+                break;
+            }
+            
+            if (double.TryParse(tempAtual, out double temperatura))
+            {
+                sensor.verificarTemperatura(temperatura);
+            }
+            else
+            {
+                Console.WriteLine("Informe um valor valido");
+            }
+        }
+    }
+}
---
+++ 5/5/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace _5;
+
+class Program
+{
+    public class DownloadEventArgs : EventArgs
+    {
+        public string FileName { get; set; }
+        public DateTime Finished { get; set; }
+
+        public DownloadEventArgs(string fileName, DateTime finished)
+        {
+            FileName = fileName;
+            Finished = DateTime.Now;
+        }
+    }
+
+    public class DownloadManager
+    {
+        public event EventHandler<DownloadEventArgs> Download;
+
+        public void iniciarDownload(string fileName)
+        {
+            Console.WriteLine("Iniciando download: " + fileName);
+
+            for (int i = 0; i <= 10; i++)
+            {
+                Thread.Sleep(500);
+                Console.WriteLine("Baixando... "+ i*10+"%");
+            }
+            
+            OnDownloadCompleted(new DownloadEventArgs(fileName, DateTime.Now));
+        }
+        protected virtual void OnDownloadCompleted(DownloadEventArgs e)
+        {
+            Download?.Invoke(this, e);
+        }
+    }
+    
+    static void Main(string[] args)
+    {
+        DownloadManager dm = new DownloadManager();
+
+        dm.Download += (sender, e) =>
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Arquivo: " + e.FileName + " baixado em: " + e.Finished);
+            Console.ResetColor();
+        };
+        
+        dm.iniciarDownload("Rock lee vs Gaara - Linkin park.mp4");
+    }
+}
